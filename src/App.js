4b8266586508e4d797da0ad5eb2c61914f2d01fc
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-04-06 02:31:42 +0100

    setup files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DateCounter from './DateCounter';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <DateCounter/>
+    </div>
+  );
+}
+
+export default App;
